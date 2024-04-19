7809ab6d3dc07a594a75dc3f778655ca21a3fb33
rootedstone <diegodiazop@gmail.com>
2024-04-18 21:58:08 -0500

    se agrego el setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mashle',
+    version='0.1',
+    packages=find_packages(),
+    url='https://github.com/gitdiegodiaz/mashle',
+    license='MIT',
+    author='Diego Diaz',
+    author_email='diegodiazop@email.com',
+    description='test',
+    python_requires='>=3.6',
+)
+
+
+
