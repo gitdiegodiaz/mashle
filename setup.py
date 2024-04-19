10869de7c7dd9993765f1b92d4c00ca6a58e252d
gitdiegodiaz <167491756+gitdiegodiaz@users.noreply.github.com>
2024-04-18 22:19:14 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 setup(
     name='mashle',
     version='0.1',
-    packages=["saludo.saludar"],
+    packages=["saludo"],
     url='https://github.com/gitdiegodiaz/mashle',
     license='MIT',
     author='Diego Diaz',
