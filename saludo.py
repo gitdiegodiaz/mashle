08f419a4c8ba84d185240b888ad394e91a3c9c23
gitdiegodiaz <167491756+gitdiegodiaz@users.noreply.github.com>
2024-04-20 09:02:37 -0500

    Update saludo.py
    se agrego clase presentacion

--- saludo.py
+++ saludo.py
@@ -1,2 +1,9 @@
 def saludar():
     print("hola mundo")
+
+
+class presentacion():
+    def __init__(self,nombre):
+        self.nombre=nombre
+    def saludar(self):
+        print (f"hola soy {self.nombre}")
