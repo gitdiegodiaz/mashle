179d7bd264c3a77d497107abdc7dd22ab6aeeab5
gitdiegodiaz <diego.diaz@movietime.com.pe>
2024-04-20 11:09:44 -0500

    cambio saludo y se agrego clase

--- saludo.py
+++ saludo.py
@@ -7,3 +7,12 @@ class presentacion():
         self.nombre=nombre
     def saludar(self):
         print (f"hola soy {self.nombre}")
+
+
+class cantante():
+    def __init__(self,letra):
+        self.letra="que bonitos ojitos tienes debajo de esas dos cejas"
+
+    def cantar(self):
+        print(f"Coro:{self.letra}")
+
